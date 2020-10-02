33e75475f04daff30b9c7bc22cc6d36accea97a0
Jonathan Treffler <mail@jonathan-treffler.de>
2020-10-02 18:24:44 +0200

    added lint:fix script

--- Rucksackproblem/pages/index.vue
+++ Rucksackproblem/pages/index.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="container">
     <div>
-      </div>
+      <md-field>
+        <label>Single</label>
+        <md-file v-model="single" />
+      </md-field>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    fileSelected (file) {
+
+    }
+  }
+}
 </script>
 
 <style>
